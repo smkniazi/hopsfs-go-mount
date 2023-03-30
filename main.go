d6c80e81e35a0b43f3cb1650acf82d2d9a0ee0c3
Salman Niazi <salman@logicalclocks.com>
2023-03-30 13:00:55 +0200

    fixed accorind to review

--- main.go
+++ main.go
@@ -161,7 +161,7 @@ func parseArgsAndInitLogger(retryPolicy *RetryPolicy) {
 	flag.IntVar(&connectors, "numConnections", 1, "Number of connections with the namenode")
 	flag.StringVar(&forceOverrideUsername, "hopsFSUserName", "", " username")
 	flag.BoolVar(&useGroupFromHopsFsDatasetPath, "getGroupFromHopsFSDatasetPath", false, "Get the group from hopsfs dataset path. This will work if a hopsworks project is mounted")
-	allowOther = *flag.Bool("allowOther", true, "Allow other users to use the filesystem")
+	flag.BoolVar(&allowOther, "allowOther", true, "Allow other users to use the filesystem")
 	flag.BoolVar(&version, "version", false, "Print version")
 
 	flag.Usage = Usage

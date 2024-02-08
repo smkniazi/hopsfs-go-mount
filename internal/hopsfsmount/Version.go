0c766977b2f19afbab85b0bf42c5606e82c02e63
Salman Niazi <salman@logicalclocks.com>
2024-02-08 12:50:37 +0100

    Bump version 3.2.0.13

--- internal/hopsfsmount/Version.go
+++ internal/hopsfsmount/Version.go
@@ -5,7 +5,7 @@ package hopsfsmount
 var (
 	// Add Version tag, manually update
 	// Note: redeploy bin to repo.hops.works
-	VERSION = "3.2.0.12"
+	VERSION = "3.2.0.13"
 
 	// GITCommit overwritten automatically by the build
 	GITCOMMIT = "HEAD"

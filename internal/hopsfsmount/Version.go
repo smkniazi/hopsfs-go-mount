9d6f4dd55999b48bc54d9134355aed3930489712
Salman Niazi <salman@logicalclocks.com>
2024-09-19 16:46:59 +0200

    Revert "HOPSFS-178: Bump version to 3.2.0.15"
    This reverts commit cf75039963daa82c6b16fde2c43c1281fe4ccbd6.

--- internal/hopsfsmount/Version.go
+++ internal/hopsfsmount/Version.go
@@ -5,7 +5,7 @@ package hopsfsmount
 var (
 	// Add Version tag, manually update
 	// Note: redeploy bin to repo.hops.works
-	VERSION = "3.2.0.15"
+	VERSION = "3.2.0.14"
 
 	// GITCommit overwritten automatically by the build
 	GITCOMMIT = "HEAD"
